49af6280138489ec3b037c7d51a5540f18cf6269
Lorenzo <l.spoleti@papers.ch>
2024-08-22 15:44:32 +0200

    chore(): update test

--- test/test.ts
+++ test/test.ts
@@ -5,10 +5,11 @@ async function testSDK() {
   const options: AcurastOracleSDKOptions = {
     wssUrls: ["wss://websocket-proxy-1.prod.gke.acurast.com", "wss://websocket-proxy-2.prod.gke.acurast.com"],
     oracles: [
-      "0x02bce6d10f4e63ef1ee9ba391c4ab495bb71b2f519d977a2e1a61f4976050debe7",
-      "0x038d8b64652b49afc3b7708691663bb2fad2e47cd9117ee2ec6fdf8734d09b67c2",
-      "0x02964a08d6e25c242f506a6262be2cd56765a22cc72c6baf1ed3992bcf02540ccb",
-      "0x03d99b4f665d5c2bb3c5497016836a54e3acf7113290f6bab35f0f829c4f7cea4a",
+      "0x022c3216c278a0e3b74eac5d245bd54421e2e49be1c72cd2619a20c297a9acc5f1",
+      "0x02ff81b36dbce74bca3ac6ff18ab52313a869347aa9d41d67c8239dea1e290bee4",
+      "0x02376180ae8bed4b9f8b7c6015e16ced9b492aa19213e9c75bed6a5c07bb1e00cf",
+      "0x0251a06befc6ba75e477a589547692b5b229551d4931fadde9a76451d8df900aa5",
+      "0x0230dd1febda2182b2b374261c7fcea9750feb5c7ea6a6eebea93fd07106b5eb92",
     ],
     logging: true,
   }
@@ -16,7 +17,7 @@ async function testSDK() {
 
   try {
     const priceParams1: FetchPricesParams = {
-      pairs: [{ from: "PEPE", to: "USDT", decimals: 6 }],
+      pairs: [{ from: "PEPE", to: "USDT", decimals: 12 }],
       protocol: "EVM" as any,
     }
 
@@ -25,7 +26,7 @@ async function testSDK() {
       protocol: "Tezos",
     }
 
-    const prices = await sdk.getPrices(priceParams1, 3)
+    const prices = await sdk.getPrices(priceParams1, 2)
     // const prices = await Promise.all([sdk.getPrices(priceParams1, 3), sdk.getPrices(priceParams2, 3)])
     console.log("Prices:", JSON.stringify(prices, null, 2))
   } catch (error) {
